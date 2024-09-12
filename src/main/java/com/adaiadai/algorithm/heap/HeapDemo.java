9bcd399f821f8f4f3c86672ad963f6bf87e9144f
adai <rottokaka@gmail.com>
2024-09-13 00:40:24 +0800

    add heap.

---
+++ src/main/java/com/adaiadai/algorithm/heap/HeapDemo.java
@@ -0,0 +1,48 @@
+package com.adaiadai.algorithm.heap;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class HeapDemo {
+
+    public static void main(String[] args) {
+        // 初始化堆
+        // 初始化小顶堆
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        // 初始化大顶堆
+        Queue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        // 元素入堆
+        maxHeap.offer(1);
+        maxHeap.offer(3);
+        maxHeap.offer(5);
+        maxHeap.offer(2);
+        maxHeap.offer(4);
+        Integer peek = maxHeap.peek();
+        // 获取堆顶元素
+        System.out.println(peek);
+
+        // 堆顶元素出堆
+        // 出堆元素会形成一个从大到小的序列
+        peek = maxHeap.poll();
+        System.out.println(peek);
+        peek = maxHeap.poll();
+        System.out.println(peek);
+        peek = maxHeap.poll();
+        System.out.println(peek);
+        peek = maxHeap.poll();
+        System.out.println(peek);
+        peek = maxHeap.poll();
+        System.out.println(peek);
+
+        // 获取堆大小
+        int size = maxHeap.size();
+        System.out.println("Heap size: " + size);
+        // 判断堆是否为空
+        boolean empty = maxHeap.isEmpty();
+        // 输入列表并建堆
+        minHeap = new PriorityQueue<>(Arrays.asList(1, 3, 5, 2, 5));
+
+    }
+}
--- src/main/java/com/adaiadai/algorithm/sorting/HeapSort.java
+++ src/main/java/com/adaiadai/algorithm/sorting/HeapSort.java
@@ -2,6 +2,15 @@ package com.adaiadai.algorithm.sorting;
 
 import java.util.Arrays;
 
+/**
+ * @author adai
+ * <p>
+ * 堆排序（heap sort）是一种基于堆数据结构实现的高效排序算法。我们可以利用已经学过的“建堆操作”和“元素出堆操作”实现堆排序。
+ * 1. 输入数组并建立小顶堆，此时最小元素位于堆顶。
+ * 2. 不断执行出堆操作，依次记录出堆元素，即可得到从小到大排序的序列。
+ * 以上方法虽然可行，但需要借助一个额外数组来保存弹出的元素，比较浪费空间。在实际中，我们通常使用一种更加优雅的实现方式。
+ * </p>
+ */
 public class HeapSort {
 
     private static void go(int[] nums) {
